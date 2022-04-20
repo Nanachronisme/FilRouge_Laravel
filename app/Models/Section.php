d538a32704b0188d01145074e31dff2f79e43f55
Nanachronisme <73257828+Nanachronisme@users.noreply.github.com>
2022-04-20 16:13:33 +0200

    authentification et autres changements
    dump de tous les autres changements fait, ne sont pas forcément cruciaux, et contient quelques fichiers oubliés dans autres commits

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'secName'
+    ];
+
+    public $timestamps = false;
 }
