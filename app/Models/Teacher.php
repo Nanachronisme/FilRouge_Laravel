36cfbc2965e5fe4ecff95a9e046aa54ac08ae3fa
Nanachronisme <73257828+Nanachronisme@users.noreply.github.com>
2022-04-20 16:07:10 +0200

    Search Function
    recherche de teachers dans la page d'index

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -22,6 +22,36 @@ class Teacher extends Model
         'section_id'
     ];
 
+
+    /**
+     * Permits to filters for searches
+     *
+     * @param $query
+     * @param array $filters
+     * @return void
+     */
+    public function scopeFilter($query, array $filters)
+    {
+        /*
+        if ($filters['search'] ?? false) //php 8 nullsafe operator, replace isset()
+        {
+            //equivalent to Post::newQuery()->filter()
+            $query
+                ->where('firstname', 'like', '%');
+        }
+        */
+
+        //searching filters using queryBuilder when() function 
+        $query->when($filters['search'] ?? false, function ($query, $search)
+        {
+            $query
+                ->where('teaFirstName', 'like', '%' . $search . '%')
+                ->orWhere('teaName', 'like', '%' . $search . '%')
+                ->orWhere('teaNickName', 'like', '%' . $search . '%');
+        });
+
+    }
+
     public function section()
     {
         return $this->belongsTo(Section::class);
