af12ad69e96b577fcfc9872773e65f18257de898
Risa <Risa@Risa.com>
2022-05-03 08:37:53 +0200

    Configuration Webpack flowbite
    contient la configuration de laravel mix pour inclure le package de flowbite.
    Compiler le résultat donnera flowbite.js dans le dossier public

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,29 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+ mix.webpackConfig({
+    stats: {
+        hash: true,
+        version: true,
+        timings: true,
+        children: true,
+        errors: true,
+        errorDetails: true,
+        warnings: false,
+        chunks: true,
+        modules: false,
+        reasons: true,
+        source: true,
+        publicPath: true,
+    }
+});
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.disableNotifications();
+
+mix.js('resources/js/app.js', 'public/js')
+.js('node_modules/flowbite/dist/flowbite.js', 'public/js')
+//.copy('node_modules/flowbite/dist/flowbite.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
