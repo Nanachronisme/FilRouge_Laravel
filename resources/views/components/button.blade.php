8abf00b903142b6dbf5ba1594ff3829ecea7e034
Risa <73257828+Nanachronisme@users.noreply.github.com>
2022-05-06 10:46:22 +0200

    Update button component to match button-link
    Component styling is now the same as the button-link component and can be used interchangeably without any visual difference in CSS

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,22 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['color' => 'black', 'type' => 'button', 'href'])
+
+@php
+    //class must be identical to the button component
+    $classes = "text-white bg-$color-700 hover:bg-$color-800 focus:ring-4 focus:outline-none 
+    focus:ring-$color-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex 
+    items-center mr-2 dark:bg-$color-600 dark:hover:bg-$color-700 dark:focus:ring-$color-800";
+
+    //Change styling for gray button
+    if ($color == 'gray') {
+        //.= operator must be used to concatenate strings
+        $classes .= " text-black border border-gray-200 hover:bg-gray-100 hover:text-blue-700";
+    }
+    elseif ($color == 'black') {
+        $classes = "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700";
+    }
+
+@endphp
+
+<button {{ $attributes->merge([ 'type' => 'submit', 'class' => $classes]) }}>
     {{ $slot }}
 </button>
