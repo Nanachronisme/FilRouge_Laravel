cb96b33def51ac3d777952f596750c22b0d64b55
Risa <73257828+Nanachronisme@users.noreply.github.com>
2022-05-06 10:43:13 +0200

    Fix button-link component
    -Fix href so it is added as a prop, and added the href in the attributes merge. href was not in the merge attributes so any links weren't added when using href as a prop
    
    -Add class styling for black buttons and

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,4 +1,3 @@
-{{-- TODO make it no longer dynamic to prevent Tailwind compiling error. Remove the color safelist afterwards --}}
 @props(['color' => 'blue', 'type' => 'button', 'href'])
 
 @php
@@ -9,12 +8,15 @@
 
     //Change styling for gray button
     if ($color == 'gray') {
-        //.= operator must be used to concatenate strings
+        //.= operator is used to concatenate strings
         $classes .= " text-black border border-gray-200 hover:bg-gray-100 hover:text-blue-700";
+    }
+    elseif ($color == 'black') {
+        $classes = "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700";
     }
 
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a {{ $attributes->merge(['href' => $href, 'class' => $classes]) }}>
     {{ $slot }}
 </a>
